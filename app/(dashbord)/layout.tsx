fa027384061aded1d011666f89aac9dbd6618fac
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2025-04-28 15:12:53 +0000

    revirement du projet

--- app/(dashbord)/layout.tsx
+++ app/(dashbord)/layout.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { AppSidebar } from "../components/app-sidebar";
 import { SidebarInset, SidebarProvider } from "../components/ui/sidebar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// smsm
 
 const queryClient = new QueryClient();
 export default function DashboardLayout({
